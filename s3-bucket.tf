97166368861ed605217f3811c807ebdb1e88d2ef
AishwaryaDevOps <aishwarya.killedar@gmail.com>
2022-01-18 07:02:41 +0000

    fixed bucket name

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3-bucket" {
   source  = "app.terraform.io/hashicat-aws-terraform/s3-bucket/aws"
   version = "2.2.0"
   # insert required variables here
-  bucket = "my-s3-bucket"
+  bucket = "my-s3-bucket-hashicat-aws"
   acl    = "private"
 
   versioning = {
